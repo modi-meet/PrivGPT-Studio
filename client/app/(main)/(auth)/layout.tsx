8fede74e3232e78d05f0eae4ad10cea0a65a8230
Modi Meet <meetmodi8080@gmail.com>
2025-10-28 17:55:10 +0530

    Move (auth) into (main)

--- client/app/(main)/(auth)/layout.tsx
+++ client/app/(main)/(auth)/layout.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-import Header from "@/components/ui/header";
-import Footer from "@/components/ui/footer";
-
 export default function AuthLayout({
   children,
 }: {
@@ -9,11 +5,9 @@ export default function AuthLayout({
 }) {
   return (
     <>
-      <Header />
       <main className="flex items-center justify-center w-full py-2">
         {children}
       </main>
-      <Footer />
     </>
   );
 }
